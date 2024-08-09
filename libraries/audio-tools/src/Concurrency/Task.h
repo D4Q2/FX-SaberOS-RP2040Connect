fd472bd0115a83b83e49616fce880fb1415c8495
RedSaber <d4quillen@icloud.com>
2024-08-09 15:49:07 -0400

    Added audio-tools library

---
+++ libraries/audio-tools/src/Concurrency/Task.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#if USE_TASK
+#ifdef ESP32
+#  include "freertos/FreeRTOS.h"
+#  include "freertos/task.h"
+#else
+#  include "FreeRTOS.h"
+#  include "task.h"
+#endif
+namespace audio_tools {
+
+/**
+ * FreeRTOS task
+ */
+class Task {
+ public:
+  Task(const char* name, int stackSize, int priority = 1, int core = -1) {
+#ifdef ESP32
+    if (core >= 0)
+      xTaskCreatePinnedToCore(task_loop, name, stackSize, this, priority,
+                              &xHandle, core);
+    else
+      xTaskCreate(task_loop, name, stackSize, this, priority, &xHandle);
+#else
+      xTaskCreate(task_loop, name, stackSize, this, priority, &xHandle);
+#endif
+
+    suspend();
+  }
+
+  ~Task() {
+    if (xHandle != NULL) {
+      vTaskDelete(xHandle);
+    }
+  }
+
+  bool begin(std::function<void()> process) {
+    LOGI("staring task");
+    loop_code = process;
+    resume();
+    return true;
+  }
+
+  /// suspends the task
+  void end() {
+    suspend();
+  }
+
+  void suspend() { vTaskSuspend(xHandle); }
+
+  void resume() { vTaskResume(xHandle); }
+
+ protected:
+  TaskHandle_t xHandle = nullptr;
+  std::function<void()> loop_code = nop;
+
+  static void nop() { delay(100); }
+
+  static void task_loop(void* arg) {
+    Task* self = (Task*)arg;
+    while (true) {
+      self->loop_code();
+    }
+  }
+};
+
+}
+
+#endif
